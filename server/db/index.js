1f602b63006ecfecf5b63b3976d56ddfe80fe07e
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-05 16:10:46 -0600

    real first working version

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,15 @@
-const db = require("./db");
-const Review = require("./models/review");
-const Cart = require("./models/cart");
+const db = require('./db')
+const Review = require('./models/Review')
+const Cart = require('./models/Cart')
+const User = require('./models/User')
+const Product = require('./models/Product')
 
 // register models
-require("./models");
+require('./models')
 
-Review.belongsTo(User, { as: "UserReview" });
-Product.hasMany(Review, { as: "ProductReview" });
-Cart.belongsTo(User, { as: "UserCart" });
-Cart.hasMany(Product, { as: "CartProduct" });
+Review.belongsTo(User, {as: 'UserReview'})
+Product.hasMany(Review, {as: 'ProductReview'})
+Cart.belongsTo(User, {as: 'UserCart'})
+Cart.hasMany(Product, {as: 'CartProduct'})
 
-module.exports = db;
+module.exports = db
