d9e411e1695f9cce34b07a3189066e53f47c1517
Avaree <avaree.warrick@gmail.com>
2019-11-05 17:00:51 -0600

    removed duplicate associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,6 @@
 const db = require('./db')
-const Review = require('./models/Review')
-const CartProduct = require('./models/CartLineItem')
-const User = require('./models/User')
-const Product = require('./models/Product')
 
 // register models
 require('./models')
-
-User.belongsToMany(Product, {through: 'UserProduct'})
-Product.belongsTo(User, {through: 'UserProduct'})
-Review.belongsTo(User, {through: 'UserReview'})
-Product.belongsToMany(Review, {through: 'ProductReview'})
-CartProduct.belongsTo(User, {through: 'UserCartProduct'})
-CartProduct.belongsToMany(Product, {through: 'CartProduct'})
 
 module.exports = db
