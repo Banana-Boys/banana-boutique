c0402cce732333d5135be346cd336b7bd054a417
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-05 11:58:44 -0600

    created product component skeletons

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,5 @@
 const User = require('./user');
-const Product = require('/product');
+const Product = require('./product');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
