b65231317c94634823d28a3c24a5e805fc467bd1
Zachary Waxman <zwaxman@gmail.com>
2019-11-05 16:53:36 -0600

    Add associations between db models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,36 @@
 const User = require('./User')
 const Product = require('./Product')
 const Order = require('./Order')
+const CartLineItem = require('./CartLineItem')
+const Category = require('./Category')
+const Address = require('./Address')
+const OrderLineItem = require('./OrderLineItem')
+const Review = require('./Review')
+
+User.hasOne(Address, { as: "defaultShipping" });
+User.hasOne(Address, { as: "defaultBilling" });
+User.hasMany(Address, { as: "addresses" });
+User.hasMany(Review);
+User.hasMany(CartLineItem);
+User.hasMany(Order);
+
+Product.hasMany(Category, {through: 'ProductCategory'})
+Product.hasMany(Review)
+Product.hasMany(OrderLineItem)
+Product.hasMany(CartLineItem)
+
+Order.belongsTo(User, {as: 'buyer'})
+Order.belongsTo(User, {as: 'receiver'})
+Order.hasMany(OrderLineItem)
+Order.hasOne(Address, {as: 'shipping'})
+Order.hasOne(Address, {as: 'billing'})
+
+Category.hasMany(Product, {through: 'ProductCategory'})
+
+Review.belongsTo(User)
+Review.belongsTo(Product)
+
+
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -23,5 +53,10 @@ const Order = require('./Order')
 module.exports = {
   User,
   Product,
-  Order
+  Order,
+  CartLineItem,
+  Category,
+  Address,
+  OrderLineItem,
+  Review
 }
