12a4a1a03c69d8360b4e4ad052dafc5a3835269e
Avaree <avaree.warrick@gmail.com>
2019-11-05 15:04:04 -0600

    made routes for review and cart

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 module.exports = router;
 
-const Review = require("../db/models/review");
-const Cart = require("../db/models/cart");
-const User = require("../db/models/user");
+// const Review = require("../db/models/review");
+// const Cart = require("../db/models/cart");
+// const User = require("../db/models/user");
 
-//mount on api
-router.use("/reviews", require("../api/reviews"));
-router.use("/cart", require("../api/cart"));
-router.use("/users", require("../api/users"));
+// //mount on api
+// router.use("/reviews", require("../api/reviews"));
+// router.use("/cart", require("../api/cart"));
+// router.use("/users", require("../api/users"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
