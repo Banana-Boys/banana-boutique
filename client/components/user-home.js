7f6f7bc332b65e18aa6398ec527e7e06c37e35fd
Zachary Waxman <zwaxman@gmail.com>
2019-11-05 11:50:20 -0600

    Fleshed out User model, added some other user attributes to display on the User component

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -6,11 +6,20 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {name, email, phone, imageUrl} = props
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h3>Welcome, {name}</h3>
+      <img src={imageUrl} />
+      <h5>Email: {email}</h5>
+      <h5>Phone #: {phone}</h5>
+      <h5>Phone #: {phone}</h5>
+      <h5>Billing Address: </h5>
+      <h5>Shipping Address: </h5>
+      <h5>Wishlists: </h5>
+      <h5>Orders: </h5>
+      <h5>Reviews: </h5>
     </div>
   )
 }
