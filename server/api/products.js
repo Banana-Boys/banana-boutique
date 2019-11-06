2731ea04dd171810d6f94b7f0564583f1bbe7c54
el-fer <fer@innovaresip.com>
2019-11-06 17:11:13 -0600

    removed test put request

--- server/api/products.js
+++ server/api/products.js
@@ -38,18 +38,6 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.put('/', async (req, res, next) => {
-  try {
-    console.log('req.body:', req.body)
-    const products = await Product.findAll({
-      include: [Review, Category]
-    })
-    res.json(products)
-  } catch (error) {
-    next(error)
-  }
-})
-
 router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.id, {
