d783a653388065cee7fb30df563eeb5bab811e81
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-07 18:32:27 -0600

    logo image

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <img src="/images/banana-boutique.png" />
     <nav>
       {isLoggedIn ? (
         <div>
@@ -19,6 +19,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
