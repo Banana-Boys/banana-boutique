1d8ca2fec14a9c9d808009e96e2a3627707dafa5
Avaree <avaree.warrick@gmail.com>
2019-11-13 08:35:38 -0600

    fixed review link that took you to product on delete

--- client/components/UsersReview.js
+++ client/components/UsersReview.js
@@ -19,70 +19,70 @@ const UsersReview = props => {
 
   const product = props.review.product || {}
   return (
-    <Link to={`/products/${product.id}`}>
-      <Container className="review-container">
-        <Comment>
-          <Comment.Avatar id="user-home-profilepic" src={product.imageUrl} />
-          <Comment.Content>
+    <Container className="review-container">
+      <Comment>
+        <Comment.Avatar id="user-home-profilepic" src={product.imageUrl} />
+        <Comment.Content>
+          <Link to={`/products/${product.id}`}>
             <Comment.Author as="a">{product.name}</Comment.Author>
-            <Comment.Metadata>
-              <div>
-                Rating:{' '}
-                {stars.map(s => (
-                  <img
-                    key={s}
-                    className="bananarating"
-                    src="https://nanas-image-store.s3.us-east-2.amazonaws.com/nana-icon.jpg"
-                  />
-                ))}
-              </div>
+          </Link>
+          <Comment.Metadata>
+            <div>
+              Rating:{' '}
+              {stars.map(s => (
+                <img
+                  key={s}
+                  className="bananarating"
+                  src="https://nanas-image-store.s3.us-east-2.amazonaws.com/nana-icon.jpg"
+                />
+              ))}
+            </div>
 
-              <Comment.Text id="reivewmetadataholder">
-                <div id="reviewbuttons">
-                  {props.user.role === 'admin' ||
-                  props.user.id === review.userId ? (
-                    <Link
-                      to={`/products/${review.productId}/reviews/${
-                        review.id
-                      }/edit`}
-                    >
-                      <Button color="blue" type="button" size="mini">
-                        Edit
-                      </Button>
-                    </Link>
-                  ) : null}
-
-                  {props.user.role === 'admin' ||
-                  props.user.id === review.userId ? (
-                    <Button
-                      color="red"
-                      type="button"
-                      size="mini"
-                      onClick={() => {
-                        props.destroyReview(review.productId, review.id)
-                      }}
-                    >
-                      Delete
+            <Comment.Text id="reivewmetadataholder">
+              <div id="reviewbuttons">
+                {props.user.role === 'admin' ||
+                props.user.id === review.userId ? (
+                  <Link
+                    to={`/products/${review.productId}/reviews/${
+                      review.id
+                    }/edit`}
+                  >
+                    <Button color="blue" type="button" size="mini">
+                      Edit
                     </Button>
-                  ) : null}
-                </div>
-              </Comment.Text>
-            </Comment.Metadata>
-            <Comment.Text>
-              <div>
-                <h5 id="reviewtitle">{review.title}</h5>
-                <p>{review.body}</p>
-                <div className="reviewbuttonsanddata">
-                  <Comment.Metadata>
-                    {dateformat(review.createdAt)}
-                  </Comment.Metadata>
-                </div>
+                  </Link>
+                ) : null}
+
+                {props.user.role === 'admin' ||
+                props.user.id === review.userId ? (
+                  <Button
+                    color="red"
+                    type="button"
+                    size="mini"
+                    onClick={() => {
+                      props.destroyReview(review.productId, review.id)
+                    }}
+                  >
+                    Delete
+                  </Button>
+                ) : null}
               </div>
             </Comment.Text>
-          </Comment.Content>
-        </Comment>
-      </Container>
-    </Link>
+          </Comment.Metadata>
+          <Comment.Text>
+            <div>
+              <h5 id="reviewtitle">{review.title}</h5>
+              <p>{review.body}</p>
+              <div className="reviewbuttonsanddata">
+                <Comment.Metadata>
+                  {dateformat(review.createdAt)}
+                </Comment.Metadata>
+              </div>
+            </div>
+          </Comment.Text>
+        </Comment.Content>
+      </Comment>
+    </Container>
   )
 }
 
